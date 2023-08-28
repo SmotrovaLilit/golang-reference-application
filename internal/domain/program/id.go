9a3fc427465bd1225d9df9088da35bfbc136d1a7
Lilit Smotrova <l.smotrova@omp.ru>
2023-08-28 19:15:18 +0400

    Fix program id comments

--- internal/domain/program/id.go
+++ internal/domain/program/id.go
@@ -5,18 +5,18 @@ import (
 )
 
 var (
-	// NewID is a constructor for program ID.
+	// NewID is a constructor for ID.
 	// Example usage:
 	//  newID, err := NewID("00000000-0000-0000-0000-000000000000")
 	NewID = id.New[*ID]
-	// MustNewID is a constructor for program ID.
+	// MustNewID is a constructor for ID.
 	// It panics if the given raw string is invalid.
 	// Example usage:
 	//  newID := MustNew("00000000-0000-0000-0000-000000000000")
 	MustNewID = id.MustNew[*ID]
 )
 
-// ID is a type for program id.
+// ID is a type for id.
 type ID struct {
 	id.Base
 }
