476561315401065766cecc5069ab05c9a91f1e25
Lilit Smotrova <l.smotrova@omp.ru>
2023-09-10 18:04:52 +0400

    Approve program version: transport layer
    See https://github.com/SmotrovaLilit/golang-reference-application/issues/20

--- internal/application/endpoints.go
+++ internal/application/endpoints.go
@@ -2,6 +2,7 @@ package application
 
 import (
 	"github.com/google/wire"
+	"reference-application/internal/application/commands/approveprogramversion"
 	"reference-application/internal/application/commands/createprogram"
 	"reference-application/internal/application/commands/sendtoreviewprogramversion"
 	"reference-application/internal/application/commands/updateprogramversion"
@@ -11,6 +12,7 @@ type Endpoints struct {
 	CreateProgramEndpoint              createprogram.Endpoint
 	UpdateProgramVersionEndpoint       updateprogramversion.Endpoint
 	SendToReviewProgramVersionEndpoint sendtoreviewprogramversion.Endpoint
+	ApproveProgramVersionEndpoint      approveprogramversion.Endpoint
 }
 
 var Set = wire.NewSet(
---
+++ internal/infrastructure/transports/http/approve_program_version.go
@@ -0,0 +1,31 @@
+package http
+
+import (
+	"context"
+	"github.com/go-kit/kit/endpoint"
+	kithttp "github.com/go-kit/kit/transport/http"
+	"github.com/gorilla/mux"
+	"net/http"
+	"reference-application/internal/application/commands/approveprogramversion"
+	"reference-application/internal/domain/version"
+	xhttp "reference-application/internal/pkg/http"
+)
+
+// newApproveProgramVersionHandler creates a new http.Handler to send to approve a version.
+func newApproveProgramVersionHandler(e approveprogramversion.Endpoint) http.Handler {
+	return kithttp.NewServer(
+		endpoint.Endpoint(e),
+		decodeApproveProgramVersionRequest,
+		xhttp.NoContentResponseEncoder,
+		handlersOptions...,
+	)
+}
+
+// decodeApproveProgramVersionRequest decodes a request to send to approve a version.
+func decodeApproveProgramVersionRequest(ctx context.Context, req *http.Request) (interface{}, error) {
+	id, err := version.NewID(mux.Vars(req)["id"])
+	if err != nil {
+		return nil, err
+	}
+	return approveprogramversion.NewCommand(id), nil
+}
