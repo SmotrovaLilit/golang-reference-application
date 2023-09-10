4ab2d954e96ae8fc80e85e5eed6a9db8b91d64d8
Lilit Smotrova <l.smotrova@omp.ru>
2023-09-10 18:28:52 +0400

    Decline program version: transport layer
    See https://github.com/SmotrovaLilit/golang-reference-application/issues/21

---
+++ internal/infrastructure/transports/http/decline_program_version_test.go
@@ -0,0 +1,56 @@
+package http
+
+import (
+	"context"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"reference-application/internal/application/commands/declineprogramversion"
+	"reference-application/internal/domain/version"
+	"testing"
+)
+
+func Test_decodeDeclineProgramVersionRequest(t *testing.T) {
+	tests := []struct {
+		name    string
+		request *http.Request
+		wantErr error
+		want    interface{}
+	}{
+		{
+			name: "valid request",
+			request: mux.SetURLVars(httptest.NewRequest(
+				http.MethodPut,
+				"/versions/11a111cf-91f3-49dc-bb6d-ac4235635411/sendToReview",
+				nil,
+			), map[string]string{"id": "11a111cf-91f3-49dc-bb6d-ac4235635411"}),
+			wantErr: nil,
+			want: declineprogramversion.NewCommand(
+				version.MustNewID("11a111cf-91f3-49dc-bb6d-ac4235635411"),
+			),
+		},
+		{
+			name: "invalid version id",
+			request: mux.SetURLVars(httptest.NewRequest(
+				http.MethodPut,
+				"/versions/11a111cf-91f3-49dc-bb6d-ac4235635411/sendToReview",
+				nil,
+			), map[string]string{"id": "invalid id"}),
+			wantErr: version.ErrInvalidID,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Run(tt.name, func(t *testing.T) {
+				got, err := decodeDeclineProgramVersionRequest(context.TODO(), tt.request)
+				if tt.wantErr == nil {
+					require.NoError(t, err)
+					require.Equal(t, tt.want, got)
+				} else {
+					require.ErrorIs(t, err, tt.wantErr)
+				}
+			})
+		})
+	}
+}
---
+++ test/integration/decline_program_version_test.go
@@ -0,0 +1,40 @@
+package integration
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"reference-application/internal/infrastructure/repositories"
+	"reference-application/internal/pkg/tests"
+	"testing"
+)
+
+func TestDeclineProgramVersionHandler(t *testing.T) {
+	// Prepare
+	test := tests.PrepareIntegrationTest(t)
+	existingVersion := test.PrepareVersionOnReview(t)
+
+	// Test operation
+	req, err := http.NewRequest(
+		"PUT",
+		test.Addr+fmt.Sprintf("/versions/%s/decline", existingVersion.ID().String()),
+		nil,
+	)
+	require.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	req = req.WithContext(context.TODO())
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		resp.Body.Close()
+	})
+	require.Equal(t, http.StatusNoContent, resp.StatusCode)
+
+	// Test assertions
+	versionRepository := repositories.NewVersionRepository(test.DB)
+	_version := versionRepository.FindByID(context.TODO(), existingVersion.ID())
+	require.NotNil(t, _version)
+	require.True(t, _version.Status().IsDeclined())
+}
